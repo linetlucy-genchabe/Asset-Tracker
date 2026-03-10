8267e3ff4264ffd6c08285597ac0c9cebf299214
Linet Lucy Genchabe <lgenchabe@livinggoods.org>
2026-03-10 18:34:44 +0300

    Add PWA support with manifest, service worker and icons

--- asset_tracker/urls.py
+++ asset_tracker/urls.py
@@ -1,29 +1,18 @@
-"""
-URL configuration for asset_tracker project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from django.views.generic.base import RedirectView
 from django.templatetags.static import static
+from django.views.static import serve
+import os
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('assetapp.urls')),
     path('login/',  auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('favicon.ico', RedirectView.as_view(url='/static/images/favicon.png', permanent=True)),
+    path('favicon.ico', RedirectView.as_view(url=static('images/favicon.png'), permanent=True)),
+    path('sw.js', serve, {'document_root': settings.BASE_DIR / 'assetapp/static', 'path': 'sw.js'}),
+    path('manifest.json', serve, {'document_root': settings.BASE_DIR / 'assetapp/static', 'path': 'manifest.json'}),
 ]
